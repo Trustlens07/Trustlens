393eaae6c250a1c9bf565c93e8f39a655d0c012f
Pallavi-1506 <pallavin250575@gmail.com>
2026-04-07 20:54:46 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from sqlalchemy import text
-from app.core.config import settings
+from core.config import settings
 from app.core.logging import setup_logging
 from app.api.v1.router import api_router
 from app.middlewares.error_handler import add_exception_handlers
